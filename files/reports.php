e32e4bc5344382e06ac847b386f6cd8cd97b024c
ivanwalker524 <ivanwalker524@gmail.com>
2022-08-18 22:06:56 +0300

    grades

--- files/reports.php
+++ files/reports.php
@@ -1,41 +1,57 @@
+<?php
+//Getting gradings for students
+function grade($score)
+{
+    if ($score >= 65) $grade = 'D1';
+    else if ($score <= 64 && $score >= 60) $grade = 'D2';
+    else if ($score <= 60 && $score >= 58) $grade = 'C3';
+    else if ($score <= 58 && $score >= 55) $grade = 'C4';
+    else if ($score <= 54 && $score >= 46) $grade = 'C5';
+    else if ($score <= 39 && $score >= 30) $grade = 'C6';
+    else if ($score <= 25 && $score >= 20) $grade = 'P7';
+    else $grade = 'F9';
+    return $grade;
+}
+?>
 <div class="wd mx-auto">
     <p style="font-size: 20px; padding: 10px 0;">Print report cards</p>
     <div class="flex" style="padding: 5px; margin-bottom: 20px;">
         <?php
         $q = $db->query("SELECT * FROM classes");
-        while($d = $q->fetch_assoc()) {
-            ?>
-            <a href="./?nav=reports&printreport=<?=$d['id']?>" style="background-color: #3a7; margin-right: 5px;padding: 5px;color: #fff;border-radius: 3px;"><?=$d['class']?></a>
-            <?php
+        while ($d = $q->fetch_assoc()) {
+        ?>
+            <a href="./?nav=reports&printreport=<?= $d['id'] ?>" style="background-color: #3a7; margin-right: 5px;padding: 5px;color: #fff;border-radius: 3px;"><?= $d['class'] ?></a>
+        <?php
         }
         ?>
     </div>
     <hr>
     <?php
-    if(isset($_GET['printreport'])) {
+    if (isset($_GET['printreport'])) {
         $classId = $_GET['printreport'];
-        $sql = "SELECT s.id, u.fname, u.lname, c.class FROM student as s join user as u on u.id=s.userid join classes as c on c.id = s.class WHERE s.class = '$classId'";
-        // echo $sql;
+        $sql = "SELECT s.id, u.fname, u.lname, c.class FROM student as s join user as u on u.id=s.userid
+        join classes as c on c.id = s.class WHERE s.class = '$classId'";
+        //echo $sql;
         $q = $db->query($sql) or die($db->error);
-        while($student = $q->fetch_assoc()) {
+        while ($student = $q->fetch_assoc()) {
             $studentId = $student['id'];
             // echo $student['fname'].'<br>';
-            ?>
+    ?>
             <main>
                 <div class="heading">
-                        <h1>BLANK REPORT CARD</h1>
-                        <form action="#">
-                            <label for="">Name: <?=$student['fname'] . ' '. $student['lname']?></label>
-                            <input type="text"><br>
-                            <label for="">Class: <?=$student['class']?></label>
-                            <input type="text"><br>
-                            <label style="color:black;" for="">Section:</label>
-                            <input type="text"><br>
-                            <label for="">Teacher:</label>
-                            <input type="text">
-                            <div class="ltable_section">
-                                <table>
-                                    <thead>
+                    <h1>BLANK REPORT CARD</h1>
+                    <form action="#">
+                        <label for="">Name: <?= $student['fname'] . ' ' . $student['lname'] ?></label>
+                        <input type="text"><br>
+                        <label for="">Class: <?= $student['class'] ?></label>
+                        <input type="text"><br>
+                        <label style="color:black;" for="">Section:</label>
+                        <input type="text"><br>
+                        <label for="">Teacher:</label>
+                        <input type="text">
+                        <div class="ltable_section">
+                            <table>
+                                <thead>
                                     <tr>
                                         <th>Total days of School</th>
                                         <th>Days Attended</th>
@@ -53,42 +69,45 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    
-                                
-                <?php
-            // return subjects from the database
-            $sql = "SELECT id, `subject` FROM subjects WHERE classId = '$classId'";
-            $q2 = $db->query($sql);
-            while($subject = $q2->fetch_assoc()) {
-                $subjectId = $subject['id'];
-                // echo "<br>Name: " . $student['fname'] . ", subject = ". $subject['subject'];
-                $sql = "SELECT * FROM marks where classId = '$classId' and subjectId = '$subjectId' and studentId = '$studentId'";
-                // Getting the marks for this subject for this specific student
-                $q3 = $db->query($sql);
-                $d3 = $q3->fetch_assoc();
-                ?>
-                <tr>
-                    <td><?=$subject['subject']?></td>
-                    <td><?=$d3['score']?></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>
-                </table>
-                <textarea name="" id="" cols="55" rows="7">comments</textarea>
-                <label class="last" for="">Teacher's signature</label>
-                <input type="text">
-                <label for="">Parents's signature</label>
-                <input type="text">
-            </form>
-            </div>
-            </div>
-            </main>
-            <?php
+
+
+                                    <?php
+                                    // return subjects from the database
+                                    $sql = "SELECT id, `subject` FROM subjects WHERE classId = '$classId'";
+                                    $q2 = $db->query($sql);
+                                    while ($subject = $q2->fetch_assoc()) {
+                                        $subjectId = $subject['id'];
+                                        // echo "<br>Name: " . $student['fname'] . ", subject = ". $subject['subject'];
+                                        $sql = "SELECT * FROM marks where classId = '$classId' and subjectId = '$subjectId' and studentId = '$studentId'";
+                                        // Getting the marks for this subject for this specific student
+                                        $q3 = $db->query($sql) or die($db->error);
+                                        $d3 = $q3->fetch_assoc();
+
+                                        $q4 = $db->query($sql);
+                                        $d4 = $q4->fetch_assoc();
+                                    ?>
+                                        <tr>
+                                            <td><?= $subject['subject'] ?></td>
+                                            <td><?= $d3['score'] ?></td>
+                                            <td><?= grade($d3['score']) ?></td>
+                                            <td></td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                            <textarea name="" id="" cols="55" rows="7">comments</textarea>
+                            <label class="last" for="">Teacher's signature</label>
+                            <input type="text">
+                            <label for="">Parents's signature</label>
+                            <input type="text">
+                    </form>
+                </div>
+</div>
+</main>
+<?php
         }
     }
-    ?>
+?>
 </div>
